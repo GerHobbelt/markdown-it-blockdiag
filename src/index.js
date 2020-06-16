0d00b9f0a7b5d9f6a7c3dc23dd86376d5279f49c
Ger Hobbelt <ger@hobbelt.com>
2020-06-16 21:55:43 +0200

    - updated npm packages - synced with template markdown-it plugin build tooling files - rebuilt library files

--- src/index.js
+++ src/index.js
@@ -1,65 +1,63 @@
-'use strict'
-
-import urljoin from 'url-join'
-import base64 from './lib/base64'
-import utf8bytes from 'utf8-bytes'
-import pako from 'pako'
-import markdownitfence from '@kaishuu0123/markdown-it-fence'
+import urljoin from 'url-join';
+import base64 from './lib/base64';
+import utf8bytes from 'utf8-bytes';
+import pako from 'pako';
+import markdownitfence from '@kaishuu0123/markdown-it-fence';
 
 const generateSourceDefaultUrl = () => {
-  return 'https://blockdiag-api.com/'
-}
+  return 'https://blockdiag-api.com/';
+};
 
 const getImageURL = (diagType, code, generateSourceUrl) => {
-  var data = utf8bytes(code)
-  var binaryString = pako.deflate(data, { to: 'string', raw: true })
-  var deflated_code = base64.toBase64(binaryString)
+  let data = utf8bytes(code);
+  let binaryString = pako.deflate(data, { to: 'string', raw: true });
+  let deflated_code = base64.toBase64(binaryString)
     .replace(/\+/g, '-') // Convert '+' to '-'
-    .replace(/\//g, '_') // Convert '/' to '_'
-  const image_url = urljoin(generateSourceUrl, 'api', 'v1', diagType, 'inflate', deflated_code)
-  return image_url
-}
+    .replace(/\//g, '_'); // Convert '/' to '_'
+  const image_url = urljoin(generateSourceUrl, 'api', 'v1', diagType, 'inflate', deflated_code);
+  return image_url;
+};
 
 const render = (image_url) => {
-  return `<a href="${image_url}" target="_blank"><img src="${image_url}" alt="${image_url}" /></a>`
-}
+  return `<a href="${image_url}" target="_blank"><img src="${image_url}" alt="${image_url}" /></a>`;
+};
 
 const BlockdiagRender = (generateSourceUrl) => {
   return (tokens, idx, options, env) => {
-    const token = tokens[idx]
-    const diag_type = token.info.trim()
-    const code = token.content.trim()
-    const image_url = getImageURL(diag_type, code, generateSourceUrl)
-    return render(image_url)
-  }
-}
+    const token = tokens[idx];
+    const diag_type = token.info.trim();
+    const code = token.content.trim();
+    const image_url = getImageURL(diag_type, code, generateSourceUrl);
+    return render(image_url);
+  };
+};
 
 const BlockdiagValidate = (params) => {
   const diag_types = [
-    "actdiag",
-    "blockdiag",
-    "nwdiag",
-    "packetdiag",
-    "rackdiag",
-    "seqdiag"
-  ]
+    'actdiag',
+    'blockdiag',
+    'nwdiag',
+    'packetdiag',
+    'rackdiag',
+    'seqdiag'
+  ];
 
-  var type = params.trim().split(' ', 2)[0]
-  return diag_types.includes(type)
-}
+  let type = params.trim().split(' ', 2)[0];
+  return diag_types.includes(type);
+};
 
 const BlockdiagPlugin = (md, options) => {
-  options = options || {}
+  options = options || {};
 
-  var generateSourceUrl = options.generateSourceUrl || generateSourceDefaultUrl()
-  var render = options.render || md.renderer.rules.image
-  var marker = options.marker || '```'
+  let generateSourceUrl = options.generateSourceUrl || generateSourceDefaultUrl();
+  let render = options.render || md.renderer.rules.image;
+  let marker = options.marker || '```';
 
   return markdownitfence(md, 'blockdiag', {
     marker: marker,
     render: BlockdiagRender(generateSourceUrl),
-    validate: BlockdiagValidate,
-  })
-}
+    validate: BlockdiagValidate
+  });
+};
 
-module.exports = BlockdiagPlugin
+module.exports = BlockdiagPlugin;
--- test/index.js
+++ test/index.js
@@ -1,50 +1,50 @@
-import assert from 'power-assert'
-import markdownIt from 'markdown-it'
-import markdownItBlockdiag from '../dist/main'
+import assert from 'power-assert';
+import markdownIt from 'markdown-it';
+import markdownItBlockdiag from '../dist/main';
 
-const mdi = markdownIt()
-mdi.use(markdownItBlockdiag)
+const mdi = markdownIt();
+mdi.use(markdownItBlockdiag);
 
-assert(mdi.render('# Hello world').trim() === '<h1>Hello world</h1>', '# Hello world')
-assert(mdi.render('Hello world').trim() === '<p>Hello world</p>', 'Hello world')
+assert(mdi.render('# Hello world').trim() === '<h1>Hello world</h1>', '# Hello world');
+assert(mdi.render('Hello world').trim() === '<p>Hello world</p>', 'Hello world');
 
 const expectTemplate = (url, diagType, expectDeflate) => {
   return `<a href="${url}/api/v1/${diagType}/inflate/${expectDeflate}" target="_blank"><img src="${url}/api/v1/${diagType}/inflate/${expectDeflate}" alt="${url}/api/v1/${diagType}/inflate/${expectDeflate}" /></a>`;
-}
+};
 
 it('blockdiag', () => {
-  var render_text = mdi.render(`\`\`\`blockdiag
+  let render_text = mdi.render(`\`\`\`blockdiag
   {
     A -> B;
   }
   \`\`\``);
-  const expect = expectTemplate('https://blockdiag-api.com', 'blockdiag', 'q-ZSAAJHBV07BSdrILsWAA==')
-  assert(render_text === expect)
+  const expect = expectTemplate('https://blockdiag-api.com', 'blockdiag', 'q-ZSAAJHBV07BSdrILsWAA==');
+  assert(render_text === expect);
 });
 
 it('blockdiag whitespace parse', () => {
-  var render_text = mdi.render(`\`\`\` blockdiag
+  let render_text = mdi.render(`\`\`\` blockdiag
   {
     A -> B;
   }
   \`\`\``);
-  const expect = expectTemplate('https://blockdiag-api.com', 'blockdiag', 'q-ZSAAJHBV07BSdrILsWAA==')
-  assert(render_text === expect)
+  const expect = expectTemplate('https://blockdiag-api.com', 'blockdiag', 'q-ZSAAJHBV07BSdrILsWAA==');
+  assert(render_text === expect);
 });
 
 it('seqdiag', () => {
-  var render_text = mdi.render(`\`\`\`seqdiag
+  let render_text = mdi.render(`\`\`\`seqdiag
   {
     A -> B;
     A <-- B;
   }
   \`\`\``);
-  const expect = expectTemplate('https://blockdiag-api.com', 'seqdiag', 'q-ZSAAJHBV07BSdrKNtGVxfCqQUA')
-  assert(render_text === expect)
+  const expect = expectTemplate('https://blockdiag-api.com', 'seqdiag', 'q-ZSAAJHBV07BSdrKNtGVxfCqQUA');
+  assert(render_text === expect);
 });
 
 it('pre block', () => {
-  var render_text = mdi.render(`\`\`\`shell
+  let render_text = mdi.render(`\`\`\`shell
 {
   testString
 }
@@ -53,29 +53,29 @@ it('pre block', () => {
   testString
 }
 </code></pre>
-`
+`;
 
-  assert(render_text === expect)
+  assert(render_text === expect);
 });
 
 it('change url', () => {
   mdi.use(markdownItBlockdiag, { generateSourceUrl: 'http://localhost:8000' });
-  var render_text = mdi.render(`\`\`\`blockdiag
+  let render_text = mdi.render(`\`\`\`blockdiag
   {
     A -> B;
   }
   \`\`\``);
-  const expect = expectTemplate('http://localhost:8000', 'blockdiag', 'q-ZSAAJHBV07BSdrILsWAA==')
-  assert(render_text === expect)
+  const expect = expectTemplate('http://localhost:8000', 'blockdiag', 'q-ZSAAJHBV07BSdrILsWAA==');
+  assert(render_text === expect);
 });
 
 it('change Marker', () => {
-  mdi.use(markdownItBlockdiag, { marker: ':::'});
-  var render_text = mdi.render(`:::blockdiag
+  mdi.use(markdownItBlockdiag, { marker: ':::' });
+  let render_text = mdi.render(`:::blockdiag
   {
     A -> B;
   }
   :::`);
-  const expect = expectTemplate('https://blockdiag-api.com', 'blockdiag', 'q-ZSAAJHBV07BSdrILsWAA==')
-  assert(render_text === expect)
+  const expect = expectTemplate('https://blockdiag-api.com', 'blockdiag', 'q-ZSAAJHBV07BSdrILsWAA==');
+  assert(render_text === expect);
 });
